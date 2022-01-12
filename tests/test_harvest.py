dc7ef03ad9f082130b5eae4afdfd91810dd92c28
Dane Cross <dncross@iu.edu>
2022-01-12 11:00:34 -0500

    update tests

---
+++ tests/test_harvest.py
@@ -0,0 +1,34 @@
+
+
+from Harvest import *
+
+pfile = "test_data/halos_snapshot_2.hdf5.1.particles"
+
+############################
+## test get_particle_mass ##
+############################
+
+mass = get_particle_mass(pfile)
+
+assert( mass == 4.61528e+06 )
+
+
+###################
+## test get_time ##
+###################
+
+a = get_time(pfile)
+
+assert( a == 0.2 )
+
+##############################
+## test get_particle_bounds ##
+##############################
+
+bounds = get_particle_bounds(pfile)
+
+assert(bounds[0] == (0.000001, 0.000300, 0.000001))
+assert(bounds[1] == (3.105634, 2.702640, 0.970517))
+
+
+
